9eb3da8b5438ad5d36b7017247471eed14de2aa7
BaicoianuIoanSorin <baicoianuioansorin@gmail.com>
2021-03-23 07:40:44 +0200

    Chaat and log fxml window`

---
+++ src/view/ViewHandler.java
@@ -0,0 +1,73 @@
+package view;
+
+import javafx.scene.Scene;
+import javafx.scene.layout.Region;
+import javafx.stage.Stage;
+
+public class ViewHandler {
+    private Scene currentScene;
+    private Stage primaryStage;
+    private ViewModelFactory viewModelFactory;
+    private LogViewController logViewController;
+    private ChatViewController chatViewController;
+
+    public ViewHandler(ViewModelFactory viewModelFactory)
+    {
+        this.viewModelFactory = viewModelFactory;
+        currentScene = new Scene(new Region());
+    }
+
+    public void start(Stage primaryStage)
+    {
+        this.primaryStage = primaryStage;
+    }
+
+    public void openView(String window)
+    {
+        Region root = null;
+        switch (window)
+        {
+            case "Log" : root = loadLogView("LogView.fxml");
+                        break;
+            case "Chat" : root = loadChatView("ChatView.fxml");
+                        break;
+        }
+        currentScene.setRoot(root);
+        String title = "";
+        if(root.getUserData() != null)
+        {
+            title += root.getUserData();
+        }
+        primaryStage.setTitle(title);
+        primaryStage.setScene(currentScene);
+        primaryStage.setWidth(root.getPrefWidth());
+        primaryStage.setHeight(root.getPrefHeight());
+    }
+
+    public Region loadLogView(String fxmlFile)
+    {
+        Region root = null;
+        try {
+
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        return root;
+    }
+
+    public Region loadChatView(String fxmlFile)
+    {
+        Region root = null;
+        try {
+
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        return root;
+    }
+
+}
