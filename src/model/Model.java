f87cfec537cfa56fdf8ce5336cfafef289861f95
juskis4 <ljuskevicius2002@gmail.com>
2021-03-24 12:01:36 +0100

    er
    ret

--- src/model/Model.java
+++ src/model/Model.java
@@ -11,15 +11,5 @@ public interface Model extends NamedPropertyChangeSubject
   ArrayList<String> getLog();
   int getConnectedUsersInt();
   ArrayList<String> getConnectedUsers();
-<<<<<<< HEAD
-  ArrayList<String> getMessages(String activeUserName);
-  String getName();
-  int getSizeOfUsers();
-  void sendMessage(String userName, String message);
-  void addUser(String userName);
-  void removeUser(String userName);
-}
-=======
   UserList getUserList();
 }
->>>>>>> 3cdab78fa7202f1b26a4315d6fe203a59a6121f9
--- src/model/ModelManager.java
+++ src/model/ModelManager.java
@@ -67,3 +67,5 @@ public class ModelManager implements Model
   }
 }
 
+
+
