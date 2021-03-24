22fffeba1396c7d9e54271345215a59dcb4d4ae2
rycka13 <71268611+rycka13@users.noreply.github.com>
2021-03-24 12:00:32 +0100

    Update Model.java

--- src/model/Model.java
+++ src/model/Model.java
@@ -6,12 +6,12 @@ import java.util.ArrayList;
 
 public interface Model extends NamedPropertyChangeSubject
 {
-  void login(String name);
   UserList getAllUsers();
   void addLog(String log);
   ArrayList<String> getLog();
   int getConnectedUsersInt();
   ArrayList<String> getConnectedUsers();
+<<<<<<< HEAD
   ArrayList<String> getMessages(String activeUserName);
   String getName();
   int getSizeOfUsers();
@@ -19,3 +19,7 @@ public interface Model extends NamedPropertyChangeSubject
   void addUser(String userName);
   void removeUser(String userName);
 }
+=======
+  UserList getUserList();
+}
+>>>>>>> 3cdab78fa7202f1b26a4315d6fe203a59a6121f9
